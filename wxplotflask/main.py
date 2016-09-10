691cb973581ff8f70f1b00d33213eb46f4b1bed4
chris matteri <chris@dyn-129-236-210-102.dyn.columbia.edu>
2016-09-10 17:28:01 -0400

    Moved DBBinder into with statement to prevent possible resource leak if the database connection doesn't have a destructor.

--- wxplotflask/main.py
+++ wxplotflask/main.py
@@ -40,56 +40,58 @@ def hello_world():
 
     aggregate_interval = int(request.args.get('aggregateInterval'))
 
-    db_binder = weewx.manager.DBBinder(config_dict)
-    binding = config_dict['WXPlot']['data_binding']
-    archive = db_binder.get_manager(binding)
-    result = archive.getSql("SELECT min(interval) FROM {} "
-        "WHERE dateTime > {} AND dateTime <= {};".format(archive.table_name,
-            start, end))
-
-    # Convert to seconds
-    if result[0]:
-        max_archive_interval = result[0] * 60
-    else:
-    # TODO: Handle this better
-        return '{"values": [], "unit": null}'
-
-    if aggregate_interval < max_archive_interval:
-        err = {
-            "code" : 1,
-            "message" : "Aggregate less than archive interval",
-            "description" : "Aggregate must not be less than the maximum archive interval between start and stop (which was {} seconds)".format(max_archive_interval)
-        }
-        return json.dumps(err), 404
-
-    (start_vec_t, stop_vec_t, data_vec_t) = \
-        archive._getSqlVectors((start, end), request.args.get('type'), aggregate_type=request.args.get('aggregateType'),
-        aggregate_interval=aggregate_interval, unix_time_intervals=True)
-            
-    # To reduce data size, the data returned by this server does not include
-    # timestamps. Rather, it only includes values. The values must correspond
-    # to consecutive aggregate intervals, with no gaps, but the data returned
-    # by _getSqlVectors may contain gaps. Thus we must iterate through the data
-    # and insert None values for intervals that don't have data. Note that the
-    # length of the returned value array may be less than the number of aggregate
-    # intervals between start and end if there is no data for all the tail
-    # intervals. The client assumes that the values start at the start time and
-    # will handle short value arrays correctly.
-
-    values = []
-    t = start
-    for interval_start, value in zip(start_vec_t[0], data_vec_t[0]):
-        while t < interval_start:
-            values.append(None)
-            t += aggregate_interval
-        values.append(round(value, 2))
-        t += aggregate_interval
+    with weewx.manager.DBBinder(config_dict) as db_binder:
+        binding = config_dict['WXPlot']['data_binding']
+        # Note: The manager does not need to closed explicitly because the DBBinder
+        # will 
+        archive = db_binder.get_manager(binding)
+        result = archive.getSql("SELECT min(interval) FROM {} "
+            "WHERE dateTime > {} AND dateTime <= {};".format(archive.table_name,
+                start, end))
     
-    data = {
-        'values': values,
-        'unit': data_vec_t[1]
-    }
-    return json.dumps(data)
+        # Convert to seconds
+        if result[0]:
+            max_archive_interval = result[0] * 60
+        else:
+        # TODO: Handle this better
+            return '{"values": [], "unit": null}'
+    
+        if aggregate_interval < max_archive_interval:
+            err = {
+                "code" : 1,
+                "message" : "Aggregate less than archive interval",
+                "description" : "Aggregate must not be less than the maximum archive interval   between start and stop (which was {} seconds)".format(max_archive_interval)
+            }
+            return json.dumps(err), 404
+    
+        (start_vec_t, stop_vec_t, data_vec_t) = \
+            archive._getSqlVectors((start, end), request.args.get('type'), aggregate_type=request.  args.get('aggregateType'),
+            aggregate_interval=aggregate_interval, unix_time_intervals=True)
+                
+        # To reduce data size, the data returned by this server does not include
+        # timestamps. Rather, it only includes values. The values must correspond
+        # to consecutive aggregate intervals, with no gaps, but the data returned
+        # by _getSqlVectors may contain gaps. Thus we must iterate through the data
+        # and insert None values for intervals that don't have data. Note that the
+        # length of the returned value array may be less than the number of aggregate
+        # intervals between start and end if there is no data for all the tail
+        # intervals. The client assumes that the values start at the start time and
+        # will handle short value arrays correctly.
+    
+        values = []
+        t = start
+        for interval_start, value in zip(start_vec_t[0], data_vec_t[0]):
+            while t < interval_start:
+                values.append(None)
+                t += aggregate_interval
+            values.append(round(value, 2))
+            t += aggregate_interval
+        
+        data = {
+            'values': values,
+            'unit': data_vec_t[1]
+        }
+        return json.dumps(data)
 
 @app.route('/test')
 def myfunc():
