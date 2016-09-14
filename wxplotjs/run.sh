5b19b35ea1b143ab750c0bee761cdec078e11380
chris matteri <chris@chriss-MacBook-Air.local>
2016-09-13 20:32:31 -0400

    Increased delay for live-reload in run.sh to prevent page reload occuring before browserify finishes, resulting in loading of a partial script.

--- wxplotjs/run.sh
+++ wxplotjs/run.sh
@@ -6,7 +6,6 @@ shopt -s failglob
 
 cd "$(dirname "$0")"
 trap 'kill $(jobs -p)' EXIT
-# Delay is in ms and doesn't like being 0, so use 1.
-node_modules/.bin/live-reload css/ test/ dev/ --port=4000 --delay=1 &
+node_modules/.bin/live-reload css/ test/ dev/ --port=4000 &
 node_modules/.bin/nodemon -w src -x "npm" -- run build-dev &
 node app.js
