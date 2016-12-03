86a167c3cd616fc430c922f244110b9a16562ca1
Chris Matteri <chrismatteri@gmail.com>
2016-12-03 14:48:53 -0500

    Test code allows selection of multiple weather data types.

--- wxplotjs/src/test.js
+++ wxplotjs/src/test.js
@@ -4,7 +4,7 @@ const WXPlot = require('./plot.js');
 // keyboard is opened.
 const isAndroid = navigator.userAgent.match(/Android/i);
 const bodyMargin = parseFloat(getComputedStyle(document.body)['margin']);
-const plot = document.getElementById('plot');
+const plotDiv = document.getElementById('plot');
 let oldInnerWidth;
 let oldInnerHeight;
 function onResize() {
@@ -12,14 +12,14 @@ function onResize() {
       || window.innerHeight > oldInnerHeight) {
     const fixedSize = window.innerWidth > 700 && window.innerHeight > 500;
     if (fixedSize || window.innerHeight > window.innerWidth) {
-      plot.className = 'portrait';
+      plotDiv.className = 'portrait';
     } else {
-      plot.className = 'landscape';
+      plotDiv.className = 'landscape';
     }
     if (fixedSize) {
-      plot.style.height = '500px';
+      plotDiv.style.height = '500px';
     } else {
-      plot.style.height = window.innerHeight - 2*bodyMargin + 'px';
+      plotDiv.style.height = window.innerHeight - 2*bodyMargin + 'px';
     }
   }
   oldInnerWidth = window.innerWidth;
@@ -28,29 +28,132 @@ function onResize() {
 window.addEventListener("resize", onResize, false);
 onResize();
 
+const plots = {
+  tempDew: {
+    traces: [{
+      displayName: 'Temperature',
+      name: 'outTemp'
+    },
+    {
+      displayName: 'Dewpoint',
+      name: 'dewpoint'
+    }],
+    unit: '°F'
+  },
+  outHumidity: {
+    traces: [{
+      displayName: 'Humidity',
+      name: 'outHumidity'
+    }],
+    unit: '%'
+  },
+  barometer: {
+    traces: [{
+      displayName: 'Pressure',
+      name: 'barometer'
+    }],
+    unit: 'inHg'
+  },
+  windChillHeatIndex: {
+    traces: [{
+      displayName: 'Wind Chill',
+      name: 'windChill'
+    },
+    {
+      displayName: 'Heat Index',
+      name: 'heatIndex'
+    }],
+    unit: '°F'
+  },
+  windSpeed: {
+    traces: [{
+      displayName: 'Wind Speed',
+      name: 'windSpeed'
+    }],
+    unit: 'mph'
+  },
+  windDir: {
+    traces: [{
+      displayName: 'Wind Direction',
+      name: 'windDir'
+    }],
+    unit: '°'
+  },
+    radiation: {
+    traces: [{
+      displayName: 'Solar Radiation',
+      name: 'radiation'
+    }],
+    unit: 'W/m²'
+  }
+};
+
+var dataParams = {
+  archiveIntervalMinutes: 5,
+  minDataPoints: 25
+}
+
+function updateTraces(plotKey, showMinMax) {
+  let colors = ['steelblue', 'crimson'];
+  let color_index = 0;
+  plot.getTraces().map(trace => plot.removeTrace(trace));
+  plot.setYLabel(plots[plotKey].unit);
+  let aggregateTypes = showMinMax ? ['max', 'avg', 'min'] : ['avg'];
+  for (const trace of plots[plotKey].traces) {
+    for (const aggType of aggregateTypes) {
+      const URLbase = '/weewx/wxplot_binding/';
+      if (plotKey === 'windSpeed' && aggType === 'max') {
+        dataParams.url = URLbase + 'windGust'
+      } else {
+        dataParams.url = URLbase + trace.name;
+      }
+      dataParams.aggregateType = aggType;
+      const traceOptions = {
+        group: trace.displayName
+      };
+      plot.addTrace(dataParams, aggType, colors[color_index], [],
+        aggType === 'avg' ? 1.5 : 0.5, traceOptions);
+    }
+    color_index++;
+  }
+}
+
+const plotSelect = d3.select("#plot-controls");
+const plotMenu = plotSelect.append('select')
+plotMenu.on("change", () => {
+  plotKey = plotMenu.node().value;
+  updateTraces(plotKey, showMinMax);
+});
+for (const plotKey in plots) {
+  const option = plots[plotKey].traces.map(x => x.displayName).join('/');
+  plotMenu.append("option")
+      .attr('value', plotKey)
+      .text(option);
+}
+
+const minMaxCheckbox = plotSelect.append('label')
+    .text('Min/Max')
+    .append('input')
+    .attr('type', 'checkbox')
+    .on('change', () => {
+      showMinMax = minMaxCheckbox.node().checked;
+      updateTraces(plotKey, showMinMax);
+    });
+
+let plotKey = plotMenu.node().value;
+let showMinMax = minMaxCheckbox.node().checked;
+
 // See readme.md for API documentation.
 const domain = {
   start: +(new Date("1/1/2015")),
-  end: +(new Date("1/1/2016"))
+  end: +Date.now()
 };
 const domainExtent = {
   start: +(new Date("1/1/2015")), 
   end: +Date.now()
 };
 
-var dataParams = {
-  aggregateType: "max",
-  url: "/weewx/wxplot_binding/outTemp", 
-  archiveIntervalMinutes: 5,
-  minDataPoints: 25
-};
+const plot = new WXPlot(d3.select("#plot-controls"), d3.select("#plot-canvas"),
+  "America/Los_Angeles", "Temperature (F)", 5, domain, domainExtent);
 
-const wxPlot = new WXPlot(d3.select("#plot-controls"), d3.select("#plot-canvas"), "America/Los_Angeles",
-  "Temperature (F)", 5, domain, domainExtent)
-    .addTrace(dataParams, "max", "steelblue", [], 0.5);
-
-dataParams.aggregateType = "avg";
-wxPlot.addTrace(dataParams, "avg", "steelblue", [], 1.5);
-
-dataParams.aggregateType = "min";
-wxPlot.addTrace(dataParams, "min", "steelblue", [], 0.5);
+updateTraces(plotKey, showMinMax);
